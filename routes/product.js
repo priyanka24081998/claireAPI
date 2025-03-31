2c6fd1c880c608d7a714e94efae83eb51672c6c8
priyanka24081998 <priyankagoti.istosyinfotech@gmail.com>
2025-03-31 14:06:38 +0530

    error deleted

--- routes/product.js
+++ routes/product.js
@@ -15,15 +15,7 @@ router.post('/createProduct',
   upload.fields([
     { name: 'images', maxCount: 10 },
     { name: 'videos', maxCount: 3 }
-  ]),(req, res) => {
-    if (!req.file) {
-      return res.status(400).json({ message: "No file uploaded" });
-    }
-    res.json({
-      message: "File uploaded successfully",
-      url: req.file.path, 
-    });
-  },verifyToken, PC.createProduct);
+  ]),verifyToken, PC.createProduct);
 router.delete('/deleteProduct/:id',verifyToken, PC.deleteProduct);
 router.patch(
     '/updateProduct/:id',
