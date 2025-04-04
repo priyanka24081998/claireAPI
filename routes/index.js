568224b6aaad8ec82869f5e2920174fa7e038459
priyanka24081998 <priyankagoti.istosyinfotech@gmail.com>
2025-04-04 22:50:47 +0530

    login recreate

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,6 @@ var express = require('express');
 var router = express.Router();
 const AC = require('../controller/adminController');
 
-router.get("/", (req, res) => {
-    res.send("Welcome to Claire API! 🎉");
-  });
 
 // ✅ Apply JWT middleware to protect routes
 router.post('/createAdmin', AC.createAdmin);
